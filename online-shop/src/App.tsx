f94e7d942799eb465ccc78f6926b2ef06946e916
JBrunnerhtl <j.brunner1@students.htl-leonding.ac.at>
2025-08-15 11:19:04 +0200

    made the mobile change mode for the navbar

--- online-shop/src/App.tsx
+++ online-shop/src/App.tsx
@@ -4,6 +4,9 @@ function App() {
 
   const navbarElements: NavbarItems[] = [
       {id: 1,name: "Home", url: "/"},
+      {id: 2,name: "About", url: "/about"},
+      {id: 3,name: "Contact", url: "/contact"},
+
   ]
   return (
     <>
--- online-shop/src/Navbar/Navbar.tsx
+++ online-shop/src/Navbar/Navbar.tsx
@@ -1,16 +1,33 @@
 import style from "./navbar.module.css"
 import PropTypes from "prop-types"
+import { useState } from "react";
 
-function Navbar(props) {
-    const elements = props.items.map(item => <li key={item.id}><a href={item.url}>{item.name}</a></li>)
+function Navbar(props: { items: { id: number, name: string, url: string }[] }) {
+    const [showNav, setShowNav] = useState(true);
+    const mobile: boolean = innerWidth < 768;
+    const elements = props.items.map(item => <li key={item.id} className={style.liStyleInNav}><a href={item.url}
+                                                                                                 className={style.aStyleInNav} >{item.name}</a>
+    </li>);
+    const navBar = <nav className={style.navStyle}>
+                                <ul className={style.ulStyleInNav}>
+                                    {elements}
+                                </ul>
+                            </nav>;
+
     return (
-        <nav>
-            <ul>
-                {elements}
-            </ul>
-        </nav>
+        mobile ? (
+            <>
+                <button type="button" onClick={() => setShowNav(!showNav)}>
+                    Menü
+                </button>
+                {showNav ? navBar : null}
+            </>
+        ) : (
+            navBar
+        )
     );
 }
+
 Navbar.PropTypes = {
     items: PropTypes.arrayOf(PropTypes.shape({
         id: PropTypes.number,
