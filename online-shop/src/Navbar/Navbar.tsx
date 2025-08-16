3a56798fae1f325a1378beb9f9d67e9d634caed2
JBrunnerhtl <j.brunner1@students.htl-leonding.ac.at>
2025-08-16 22:44:32 +0200

    finished nav

--- online-shop/src/Navbar/Navbar.tsx
+++ online-shop/src/Navbar/Navbar.tsx
@@ -5,14 +5,27 @@ import { useState, useEffect } from "react";
 function Navbar(props: { items: { id: number, name: string, url: string }[] }) {
     const [showNav, setShowNav] = useState(false);
     const mobile: boolean = innerWidth < 768;
-    const elements = props.items.map((item) => <li key={item.id} className = {mobile ? !showNav ? style.liStyleInNavFlyOut: style.liStyleInNavMobile: style.liStyleInNav}><a href={item.url}
-                                                                                                 className={style.aStyleInNav} >{item.name}</a>
+    const homeIcon = (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path fill="#fff" d="M12 3.172L19 10.172V20H5V10.172L12 3.172ZM12 1L2 11H5V21H19V11H22L12 1Z"/>
+        </svg>
+    );
+    const exit = <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+        <path fill="#fff"
+              d="M16.192 7.757a1 1 0 0 0-1.414-1.414L12 9.121 9.222 6.343a1 1 0 1 0-1.414 1.414L10.586 10.535l-2.778 2.778a1 1 0 1 0 1.414 1.414L12 11.949l2.778 2.778a1 1 0 0 0 1.414-1.414l-2.778-2.778 2.778-2.778z"/>
+        <circle cx="12" cy="12" r="10" stroke="#fff" strokeWidth="2" fill="none"/>
+    </svg>
+
+    const elements = props.items.map((item) => <li key={item.id}
+                                                   className={mobile ? !showNav ? style.liStyleInNavFlyOut : style.liStyleInNavMobile : style.liStyleInNav}>
+        <a href={item.url}
+           className={style.aStyleInNav}>{item.name}</a>
     </li>);
     const navBar = <nav className={style.navStyle}>
-                                <ul className={style.ulStyleInNav}>
-                                    {elements}
-                                </ul>
-                            </nav>;
+        <ul className={style.ulStyleInNav}>
+            {elements}
+        </ul>
+    </nav>;
     const [isVisible, setIsVisible] = useState(false);
     useEffect(() => {
         if (showNav) {
@@ -26,11 +39,11 @@ function Navbar(props: { items: { id: number, name: string, url: string }[] }) {
     return (
         mobile ? (
             <>
-                <button type="button" onClick={() =>
-                {
+                <button type="button" className={style.buttonStyleForNavbar} onClick={() => {
                     setTimeout(() => setShowNav(prev => !prev), 100);
+
                 }}>
-                    Menü
+                    {showNav ? exit : homeIcon}
                 </button>
                 {isVisible && navBar}
             </>
